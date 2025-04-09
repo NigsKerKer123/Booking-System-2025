ecdc8c09b25fa0d4d3628b073eaee5b931cebbe8
Kirk John L. Sieras <bebekirk123@gmail.com>
2025-04-09 19:44:14 +0800

    book migration added

--- resources/views/tenant/admin/students.blade.php
+++ resources/views/tenant/admin/students.blade.php
@@ -68,7 +68,7 @@
 @endif
 
 <div class="p-4 sm:ml-64">
-   <div class="pl-4 pr-2 mt-22 flex justify-between items-center">
+   <div class="pl-4 pr-2 mt-22 flex justify-end items-center">
       <!-- add button -->
          <button type="button" data-modal-target="add-modal" data-modal-toggle="add-modal" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Add Student</button>
    </div>
